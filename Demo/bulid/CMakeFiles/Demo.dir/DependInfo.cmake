727465149716a5d97b06eda4cc8b866d06e328bb
wangrui <wangrui@originqc.com>
2021-08-16 09:18:19 +0800

    [add] add Demo

---
+++ Demo/bulid/CMakeFiles/Demo.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "F:/git_learn_with_github/Demo/main.cc" "CMakeFiles/Demo.dir/main.cc.obj" "gcc" "CMakeFiles/Demo.dir/main.cc.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "F:/git_learn_with_github/Demo/bulid/math/CMakeFiles/MathFunctions.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Demo/bulid/math/CMakeFiles/MathFunctions.dir/MathFunctions.cc.obj.d
@@ -0,0 +1,2 @@
+math/CMakeFiles/MathFunctions.dir/MathFunctions.cc.obj: \
+ F:/git_learn_with_github/Demo/math/MathFunctions.cc
